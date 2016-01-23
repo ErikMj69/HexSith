8b9afa43e3e8f1ba0e491533eba94224c6bd63d7
ErikMj69 <erik.mj69@gmail.com>
2016-01-22 23:29:52 -0600

    Upload webserver

--- web_server.js
+++ web_server.js
@@ -1,34 +1,62 @@
 /* * * * * * * * * * *  HEXSITH SERVER  * * * * * * * * * * */
 
-var http = require( 'http' ),
-    url = require( 'url' ),
-    fs = require( 'fs' );
+var port = 36912;
+var serverUrl = "127.0.0.1";
+ 
+var http = require("http");
+var path = require("path");
+var fs = require("fs");
+ 
+console.log("Starting web server at " + serverUrl + ":" + port);
+ 
+http.createServer( function(req, res) {
+ 
+  var now = new Date();
 
-var indexURL = 'app';
-    logSrc = "server/logs/";
+  var filename = req.url || "index.html";
+  var ext = path.extname(filename);
+  var localPath = 'app';
+  var validExtensions = {
+    ".html" : "text/html",
+    ".js": "application/javascript",
+    ".wec": "text/wec",
+    ".css": "text/css",
+    ".txt": "text/plain",
+    ".jpg": "image/jpeg",
+    ".gif": "image/gif",
+    ".png": "image/png"
+  };
+  var isValidExt = validExtensions[ext];
 
-var req = {};
-
-var routes = JSON.parse( fs.readFileSync( './server/config/routes.json', 'utf8' ) );
-
-http.createServer( function( request, response ) {
-
-  if( request.url !== "/" ) {
-    fs.exists( indexURL + request.url, function( exists ) {
-      if( exists )
-        req.file = fs.readFileSync( indexURL + request.url );
-      else
-        req.file = fs.readFileSync( indexURL + "/404.html" );
+  if (isValidExt) {
+    localPath += filename;
+    fs.exists(localPath, function(exists) {
+      if(exists) {
+        console.log("Serving file: " + localPath);
+        getFile(localPath, res, ext);
+      } else {
+        console.log("File not found: " + localPath);
+        res.writeHead(404);
+        res.end();
+      }
     });
-  } else
-    req.file = fs.readFileSync( indexURL + routes.initPage );
-  
-  fs.writeFile( logSrc + "error/errno00.txt", request.url, function( err ) {
-    if( err ) console.log( err );
+
+  } else {
+    console.log("Invalid file extension detected: " + ext);
+  }
+ 
+}).listen(port, serverUrl);
+ 
+function getFile(localPath, res, mimeType) {
+  fs.readFile(localPath, function(err, contents) {
+    if(!err) {
+      res.setHeader("Content-Length", contents.length);
+      res.setHeader("Content-Type", mimeType);
+      res.statusCode = 200;
+      res.end(contents);
+    } else {
+      res.writeHead(500);
+      res.end();
+    }
   });
-  response.writeHead( 200, { 'Content-Type': 'text/html' } );
-  response.end( req.file );
-
-}).listen( 36912 );
-
-console.log( "Listening in port 36912" );
+} 
